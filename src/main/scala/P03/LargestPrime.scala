7c672910076269a8701c0a7e1c003c33b7074a74
rafalkac02 <69152315+rafalkac02@users.noreply.github.com>
2023-03-04 12:14:04 +0100

    problem 03

--- src/main/scala/P03/LargestPrime.scala
+++ src/main/scala/P03/LargestPrime.scala
@@ -1,4 +1,18 @@
 package P03
 
 object LargestPrime extends App {
+
+  val n = 600851475143L
+
+  def largestPrimeFactor(b: Long) = {
+    def loop(factor: Long, n: Long): Long =
+      if (factor == n) n
+      else if (n % factor == 0) loop(factor, n / factor)
+      else loop(factor + 1, n)
+
+    loop(2L, b)
+  }
+
+  // prints 6857
+  println(largestPrimeFactor(n))
 }
