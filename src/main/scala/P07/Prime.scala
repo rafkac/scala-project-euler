8e12d998a7054a01abf2a0537edb12f6e91ef962
rafalkac02 <69152315+rafalkac02@users.noreply.github.com>
2023-03-08 13:58:19 +0100

    problem 07

--- src/main/scala/P07/Prime.scala
+++ src/main/scala/P07/Prime.scala
@@ -2,5 +2,10 @@ package P07
 
 object Prime extends App {
 
-  def isPrime(n: Int) = Range(2, n - 1).count(n % _ == 0) == 0
+  def isPrime(n: Int): Boolean = !((2 until n - 1) exists (n % _ == 0))
+
+  lazy val primes: LazyList[Int] = 2 +: LazyList.from(3).filter(isPrime)
+
+  // prints 104743
+  println(primes(10000))
 }
